7e3670f85a1f092a8a953a3f391359c5081e410a
Boston <563632216@qq.com>
2018-11-16 15:58:03 +0800

    get all workers page 1

---
+++ lb_dj_worker/spiders/quotes_spider.py
@@ -0,0 +1,32 @@
+import scrapy
+import json
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+
+    def start_requests(self):
+        urls = [
+            'https://www.lb-dj.com/website/worker/workerListNew?city=&province=&district=&category=&codeType=&thisPage=1',
+            # 'http://quotes.toscrape.com/page/1/',
+            # 'http://quotes.toscrape.com/page/2/',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        data = response.body
+        # data = data.decode('utf-8')
+        # data = data.decode('unicode-escape')
+        data = json.loads(data)
+        # self.log(data)
+        self.log( json.dumps(data, sort_keys=True, indent = 4, separators = (',', ': '), ensure_ascii=False) )
+
+
+
+    # def parse(self, response):
+    #     page = response.url.split("/")[-2]
+    #     filename = 'quotes-%s.html' % page
+    #     with open(filename, 'wb') as f:
+    #         f.write(response.body)
+    #     self.log('Saved file %s' % filename)
